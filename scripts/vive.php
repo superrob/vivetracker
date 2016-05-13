b029444a610fa807e9bccf2f74858a5958686ea4
Robin Madsen <robin@robserob.dk>
2016-05-13 12:04:48 +0200

    Main scraper - MySQLi rewrite.
    Also now using prepared statements for the very frequently used queries.

--- scripts/vive.php
+++ scripts/vive.php
@@ -8,8 +8,7 @@ function generateTrackingNumber($seq) {
 }
 
 // Find the current highest tracking number.
-$get = mysql_query("select id from vive order by ID desc limit 1");
-$startdat = mysql_fetch_array($get);
+$startdat = $db->query("select id from vive order by ID desc limit 1")->fetch_array();
 
 // Get the current sequence number from the current tracking number.
 $seq = floor($startdat['id']/70)*7;
@@ -17,6 +16,13 @@ $seq = floor($startdat['id']/70)*7;
 echo "Starting from $seq\n\n";
 // Initialize curl.
 $ch = curl_init();
+
+// Prepared statement for checking if tracking number already is present in database.
+$db_check_stmt = $db->prepare('select id from vive where id=?');
+
+// Prepared statement for inserting into the Vive table
+$db_insertVive_stmt = $db->prepare('insert into vive (id, found, origin, destination, description, firstdate) values (?, 1, ?, ?, ?, ?)');
+
 // Continue to loop until we break the loop.
 while (true) {
 	// Init an empty array for the tracking numbers.
@@ -27,8 +33,12 @@ while (true) {
 		$cur = generateTrackingNumber($seq++);	
 		echo "Trying $cur\r";		
 		
+		// Update the statement with current tracking number.
+		$db_check_stmt->bind_param("i", $cur);
+		$db_check_stmt->execute();
+		$db_check_stmt->store_result();
 		// Check if the generated tracking number is already present in the database.
-		if (mysql_num_rows(mysql_query("select id from vive where id='".$cur."'")) == 0)
+		if ($db_check_stmt->num_rows == 0)
 			$trackingNumbers[] = $cur;
 	}
 	
@@ -43,7 +53,7 @@ while (true) {
 	// Are there any results?
 	if (!isset($json["results"])) {
 		// No results.. Lets check if we have gone "too far away" from the current highest tracking number found.
-		$highestdat = mysql_fetch_array(mysql_query("select id from vive where found=1 order by ID desc limit 1"));
+		$highestdat = $db->query("select id from vive where found=1 order by ID desc limit 1")->fetch_array();
 		if ($cur - $highestdat['id'] > 10000 ) {
 			echo "No more new tracking numbers\n";
 			break;
@@ -51,9 +61,9 @@ while (true) {
 	} else {
 		// We have some results!
 		foreach ($json['results'] as $current) {			
-			$origin = mysql_real_escape_string($current["origin"]["value"]);
-			$des = mysql_real_escape_string($current["destination"]["value"]);
-			$descrip = mysql_real_escape_string($current["description"]);
+			$origin = $current["origin"]["value"];
+			$destination = $current["destination"]["value"];
+			$description = $current["description"];
 			$checks = $current["checkpoints"][0]["counter"]-1;
 			$check = $current["checkpoints"][$checks];
 			$unix = strtotime($check['date'])+3600;
@@ -63,10 +73,9 @@ while (true) {
 			//echo "\nDescription: " . $current["description"];
 			//echo "\nCheckpoint: " . $check["description"] . " at: " . $check["date"];
 			echo "Date: " . date("j m y") . "\n\n";
-			// Lets remove the tracking number from the revive table (If present)
-			mysql_query("delete from revive where id=".$current["id"]);
-			// Lets insert the newly found tracking number into the Vive table.
-			mysql_query("insert into vive (id, found, origin, destination, description, firstdate) values (".$current["id"].", 1, '".$origin."', '$des', '$descrip', FROM_UNIXTIME(".$unix."))");
+			// Lets insert the newly found tracking number into the Vive table.			
+			$db_insertVive_stmt->bind_param("issss", $current['id'], $origin, $destination, $description, date("Y-m-d", $unix));
+			$db_insertVive_stmt->execute();
 		}	
 	}
 	if (isset($json["errors"])) {
@@ -75,10 +84,14 @@ while (true) {
 			if ($current['code'] != 404)
 				print_r($current);
 			// Lets add this tracking number as not found in the Vive table.
-			mysql_query("insert into vive (id, found) values (".$current["id"].", 0)");
+			$db->query("insert into vive (id, found) values (".$current["id"].", 0)");
 			// Lets also add it to the revive tracker.
-			mysql_query("insert into revive (id) values (".$current["id"].")");
+			$db->query("insert into revive (id) values (".$current["id"].")");
 		}	
 	}
 }
+// Close the MySQL connection
+$db_check_stmt->close();
+$db_insertVive_stmt->close();
+$db->close();
 ?>
